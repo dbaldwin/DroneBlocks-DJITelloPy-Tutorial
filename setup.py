04b21f310d0e33c6dd8b968e1d984a84da02bb11
patrickryan <theyoungsoul@gmail.com>
2021-06-14 20:44:01 -0500

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='DroneBlocks-DJITelloPy-Tutorial',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Droneblocks.io',
+    author_email='',
+    description='DJITelloPy API Tutorial',
+    install_requires = [
+        'opencv-python==4.5.2.52',
+        'opencv-contrib-python==4.5.2.52',
+        'imutils==0.5.4',
+        'djitellopy2==2.3',
+    ]
+
+)
